e882b41081d1ad0149b9daf0d2466a4bb4e48505
Ugarz <ugo.arzur@gmail.com>
2017-02-21 17:48:04 +0100

    fixes components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import NotificationsSwitch from './components/notifications-switch'
+import UserSettings from './components/user-settings'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <hr />
+        <NotificationsSwitch />
+        <hr />
+        <UserSettings />
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
